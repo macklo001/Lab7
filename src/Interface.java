733fd74c32f4f4d7da24e3a67397b0eb641a3bb2
macklo001 <adres_macklo001@utp.edu.pl>
2021-05-27 18:31:43 +0200

    Lab7_2

--- src/Interface.java
+++ src/Interface.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class Interface {
+// public class Interface {
+// }
+
+import java.util.Scanner;
+
+public class Interface {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean gameOver = true;
+        System.out.println();
+        System.out.println("*************************** WITAJ W KULTOWEJ GRZE OCZKO ********************************");
+        System.out.println("******************** NA POCZĄTEK POROSZĘ ABYŚ PODAŁ ILOŚC GRACZY ( 2 - 4 ) **************");
+        System.out.println();
+        while (gameOver) {
+            System.out.println("                           1. * Black Jack * ");
+            System.out.println("                           2. Ilość graczy: 2");
+            System.out.println("                           3. Ilość graczy: 3");
+            System.out.println("                           4. Ilość graczy: 4 ");
+            System.out.println("                           5. Wyjście z programu ");
+            System.out.println("Twój wybór: ");
+            int userChoice = scanner.nextInt();
+            switch (userChoice) {
+                case 1 -> System.out.println("Prosta gra karciana - BlackJack\n" +
+                        "Przepływ programu:\n" +
+                        "1. Gra pyta o liczbę graczy (maksymalnie 4. graczy) - dodatkowo można zapytać o imię graczy.\n" +
+                        "2. Gra przygotowuje standardową talię kart ułożoną losowo (potasowaną)\n" +
+                        "3. Gra wyciąga pierwszą kartę dla gracza nr 1. i pyta, czy dobierać dalej:\n" +
+                        "    - jeśli gracz wpisze słówko \"next\", gracz otrzymuje kartę\n" +
+                        "    - jeśli gracz wpisze słówko \"pass\", rezygnuje z dobierania dalszych kart\n" +
+                        "4. W zależności jeśli gracz zrezygnuje z dobierania lub przebije sumę 21, gra przechodzi do następnego gracza.\n" +
+                        "5. Krok 3 powtarza się dla wszystkich graczy.\n" +
+                        "6. Na końcu karty dobiera komputer.\n" +
+                        "7. W zależności od wyniku program drukuje informację o zwycięzcy:\n" +
+                        "    - wygrywa ten, kto był najbliżej wyniku 21, ale go nie przekroczył\n" +
+                        "        - wyjątek: perskie BlackJack dające wynik 22\n" +
+                        "    - jeśli kilkoro graczy ma ten sam wynik, należy wydrukować informację o remisie, kto zremisował i z jakim wynikiem\n" +
+                        "8. W zależności od różnych wyników wygrywa ten, czyja kombinacja ma większy priorytet:\n" +
+                        "    - perskie BlackJack (może być remis jeśli dwóch graczy będzie je mieć),\n" +
+                        "    - standardowe BlackJack,\n" +
+                        "    - suma najbliższa 21.\n" +
+                        "9. Na zakończenie gry po informacji kto wygrał gra proponuje czy zagrać jeszcze raz " +
+                        "                                                                                          ");
+                case 2 -> System.out.println("Lczba graczy: 2");
+                case 3 -> System.out.println("Lczba graczy: 3");
+                case 4 -> System.out.println("Lczba graczy: 4");
+                case 5 -> gameOver = false;
+            }
+        }
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,120 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean gameOver = true;
+        System.out.println();
+        System.out.println("*************************** Programowanie 2 IV sem EiT NS LAB 7 ********************************");
+        System.out.println("****************************** MENU: masz do wyboru  ( 1 - 5 ) *********************************");
+        System.out.println();
+        while (gameOver) {
+            System.out.println("                           1. Wymagania dla klasy Dog ");
+            System.out.println("                           2. Wymagania dla adaptera klasy  \n" +
+                    "                               ComputerGame i interfejsu PCGame ");
+            System.out.println("                           3. Kod klasy PhoneNumberFinder ");
+            System.out.println("                           4. * Autorzy programu * ");
+            System.out.println("                           5. Wyjście z programu ");
+            System.out.println("Twój wybór: ");
+            int userChoice = scanner.nextInt();
+            switch (userChoice) {
+                case 1 -> System.out.println("Stwórz builder dla klasy, która ma następujące pola: \n" +
+                        "public class Dog { private String name; private String type; \n" +
+                        "private Integer age; private List<String> toys; } \n" +
+                        "Builder powinien zostać zdefiniowany wewnątrz klasy Dog\n" +
+                        "i \tpowinien być jedynym sposobem stworzenia jej instancji.\n ");
+                case 2 -> System.out.println("Stwórz adapter klasy ComputerGame do interfejsu PCGame. \n" +
+                        "Uwagi: \n" +
+                        "•\tliczba w nazwie wartości PegiAgeRating oznacza minimalny wiek gracza \n" +
+                        "•\tpotraktuj grę jako TripleA jeżeli jej budżet przekracza 50 milionów \n" +
+                        " \n" +
+                        "@Data \n" +
+                        "@AllArgsConstructor public class ComputerGame { private final String name; private final PegiAgeRating pegiAgeRating; private final Double budgetInMillionsOfDollars; private final Integer minimumGpuMemoryInMegabytes; private final Integer diskSpaceNeededInGB; private final Integer ramNeededInGb; private final Integer coresNeeded; private final Double coreSpeedInGhz; \n" +
+                        "} \n" +
+                        " \n" +
+                        "public enum PegiAgeRating { \n" +
+                        "  P3, P7, P12, P16, P18 \n" +
+                        "} \n" +
+                        " \n" +
+                        "public class Requirements { private final Integer gpuGb;    private final Integer HDDGb;    private final Integer RAMGb;    private final Double cpuGhz;    private final Integer coresNum; \n" +
+                        "} \n" +
+                        " \n" +
+                        "public interface PCGame { \n" +
+                        "   String getTitle();    Integer getPegiAllowedAge();   boolean isTripleAGame(); \n" +
+                        "   Requirements getRequirements(); \n" +
+                        "} \n");
+                case 3 -> System.out.println("import java.util.HashMap;\n" +
+                        "import java.util.Map;\n" +
+                        "\n" +
+                        "public class PhoneNumberFinder {\n" +
+                        "\n" +
+                        "    private static PhoneNumberFinder INSTANCE = new PhoneNumberFinder();\n" +
+                        "\n" +
+                        "    private Map<Integer, String> PhoneNumbersMap;\n" +
+                        "\n" +
+                        "    private PhoneNumberFinder(){\n" +
+                        "        PhoneNumbersMap = NumbersDB();\n" +
+                        "    }\n" +
+                        "\n" +
+                        "    public static PhoneNumberFinder getInstance() {\n" +
+                        "        return INSTANCE;\n" +
+                        "    }\n" +
+                        "\n" +
+                        "    public String FindNumber(Integer number){\n" +
+                        "        return PhoneNumbersMap.get(number);\n" +
+                        "    }\n" +
+                        "\n" +
+                        "    private Map<Integer, String> NumbersDB() {\n" +
+                        "\n" +
+                        "\n" +
+                        "        return new HashMap<Integer, String>() {{\n" +
+                        "            put(727247452, \"Adamiak Adam\");\n" +
+                        "            put(663774923, \"Rywal Damian\");\n" +
+                        "            put(987329127, \"Borysewicz Borys\");\n" +
+                        "            put(632801222, \"Piekna Ola\");\n" +
+                        "            put(312142322, \"Dorodna Agata\");\n" +
+                        "            put(888321032, \"Zdrowa Ania\");\n" +
+                        "            put(888210432, \"Zdun Piotr\");\n" +
+                        "            put(623341322, \"Kawiorski Jan\");\n" +
+                        "\n" +
+                        "        }};\n" +
+                        "    }\n" +
+                        "}");
+                case 4 -> System.out.println("Maciej Klonowski nr albumu 115612 \n" +
+                        "Bartosz Babiarz nr albumu 109627 \n");
+                case 5 -> gameOver = false;
+            }
+        }
+    }
 
-        public static void main(String[] args) {
+ //   public static void main(String[] args) {
 
-            //Zad 1 Lab 7
+        //Zad 1 Lab 7
 
-            Dog.DogBuilder dogBuilder = Dog.DogBuilder.aDog();
-            Dog Reksio = dogBuilder.withName("Reksio").withAge(2).withType("Cane Corso").build();
-            Dog Lady = dogBuilder.withName("Lady").withAge(4).withToys(Arrays.asList("kaczka", "drapak")).build();
-
-            System.out.println(Reksio);
-            System.out.println(Lady);
+     //   Dog.DogBuilder dogBuilder = Dog.DogBuilder.aDog();
+       // Dog Reksio = dogBuilder.withName("Reksio").withAge(2).withType("Cane Corso").build();
+     //   Dog Lady = dogBuilder.withName("Lady").withAge(4).withToys(Arrays.asList("kaczka", "drapak")).build();
+}
+    //        System.out.println(Reksio);
+   //         System.out.println(Lady);
 
 
             //Zad 2 Lab 7
 
-            ComputerGame Settlers = new ComputerGame("Settlers", ComputerGame.PegiAgeRating.P12, 20.0d, 1032,
-                    20, 2, 2, 1.2d);
-            System.out.println("Name: " + Settlers.getTitle());
-            System.out.println("TripleA: " + Settlers.isTripleAGame());
-            System.out.println("Allowed age: " + Settlers.getPegiAllowedAge());
-            System.out.println("Requirements: " + Settlers.getRequirements());
+//           ComputerGame Settlers = new ComputerGame("Settlers", ComputerGame.PegiAgeRating.P12, 20.0d, 1032,
+          //          20, 2, 2, 1.2d);
+  //         System.out.println("Name: " + Settlers.getTitle());
+    //       System.out.println("TripleA: " + Settlers.isTripleAGame());
+      //     System.out.println("Allowed age: " + Settlers.getPegiAllowedAge());
+        //  System.out.println("Requirements: " + Settlers.getRequirements());
 
 
             //Zad 3 Lab 7 "Uzyty wzorzec - SINGLETON"
 
-            PhoneNumberFinder PhoneFinder = PhoneNumberFinder.getInstance();
-            System.out.println("Szukany posiadacz numeru  " + PhoneFinder.FindNumber(727247452));
+      //   PhoneNumberFinder PhoneFinder = PhoneNumberFinder.getInstance()
+     //    System.out.println("Szukany posiadacz numeru  " + PhoneFinder.FindNumber(727247452));
 
 
 
-        }
-    }
 
